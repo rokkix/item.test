21ac1b21a0ac9783acf84bb02e83d6b5643aae7c
Yury Firsanau <rokki@tut.by>
2017-05-20 14:28:57 +0200

    cache catalog and product

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use Illuminate\Support\Facades\Cache;
 use App\Product;
 use App\Repositories\ProductsRepository;
 use Illuminate\Http\Request;
@@ -22,7 +22,12 @@ class IndexController extends Controller
     {
         $sort = $request->only('sort');
 
-        $catalogs = $this->getProducts($sort);
+        $catalogs = Cache::rememberForever('sort:' . implode($request->only('sort', 'page')), function () use ($sort) {
+            $res = $this->getProducts($sort);
+            return $res;
+
+        });
+
 
         if (isset($sort['sort'])) {
 
@@ -35,8 +40,13 @@ class IndexController extends Controller
 
     public function show($slug)
     {
+        $product = Cache::rememberForever('product:'.$slug, function () use ($slug) {
+            $res = $this->p_rep->one($slug);
+            return $res;
 
-        $product = $this->p_rep->one($slug);
+        });
+
+
         return view(env('THEME') . '.product')->with('product', $product);
 
     }
