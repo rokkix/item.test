f2f82bc68d4c7bd363758a46a88d321b18001b63
Yury Firsanau <rokki@tut.by>
2017-05-20 20:11:02 +0200

    remark name class seede

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ProductsTableSeeder::class);
-        $this->call(DatabaseSeeder::class);
+        $this->call(check_cacheTableSeeder::class);
     }
 }
